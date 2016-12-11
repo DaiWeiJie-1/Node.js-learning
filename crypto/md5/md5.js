a45e8b00f68ddb854e7f031ff525ed6eb334f824
daiweijie <690385484@qq.com>
2016-12-11 10:58:09 +0800

    提交crypto-md5

--- crypto/md5/md5.js
+++ crypto/md5/md5.js
@@ -5,7 +5,10 @@ const hash = crypto.createHash('md5');
 hash.update('Hello,world!');
 hash.update('Hello,nodejs');
 
+//结果用16进制显示
 // var hexHashVal = hash.digest('hex');
+
+//结果用base64表示
 var base64HashVal = hash.digest('base64');
 
 // console.log("hex:" + hexHashVal);
