1885de3becfb5b561c922f931062b471ffb9ee08
daiweijie <daiweijie@danale.com>
2016-12-14 21:01:38 +0800

    提交fs训练demo-过滤字符串

--- express/处理文件demo(按格式提取字符)/app.js
+++ express/处理文件demo(按格式提取字符)/app.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var readStream = fs.createReadStream('strings.xml','utf-8');
 var writeStream = fs.createWriteStream('out-string.xml','utf-8');
-var stringArray = new Array();
+var objs = {};
 readStream.on('data',function(chunk){
     var arry = chunk.split('\n');
     for(var pos in arry){
@@ -11,10 +11,21 @@ readStream.on('data',function(chunk){
         var endIndex = targetStr.indexOf('</string>')
         if(firstIndex != -1 && endIndex != -1){
             var str = targetStr.substring(firstIndex+1,endIndex);
-            console.log(str);
-            writeStream.write(str + "\n");
+            var num = objs[str];
+            if(num != undefined){
+                objs[str] = objs[str]+1;
+            }else{
+                objs[str] = 1;
+            }
         }
-        // console.log('text : ' + arry[pos]);
+    }
+
+    for(var key in objs){
+         if(objs[key] > 1){
+             //输出内容重复的字段
+             console.log(key + ":" + objs[key]);
+         }
+         writeStream.write(key + "\n");
     }
     writeStream.end();
 });
